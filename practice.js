41f19d4003e0a05b29b6c9691494824efef3dfb4
Yuya Hochi <yuya1charo@gmail.com>
2022-06-26 22:06:31 +0900

    remove node_modules

--- practice.js
+++ practice.js
@@ -1,117 +1,92 @@
-// Hash Search with Hash Table.
+// function
+// merge, quick, bubble, insertion
 
-// This is forward linked list. It is absolutely no point in making
-// this with doubly linked list.
-
-class Node {
-  constructor(key, val) {
-    this.key = key;
-    this.val = val;
-    this.next = null;
+//insertion sort
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > arr[i]) {
+        const temp = arr[j];
+        arr[j] = arr[i];
+        arr[j + 1] = temp;
+      }
+    }
   }
-}
-class HashTable {
-  constructor(size) {
-    // The size of a HashTable varies depending on how much data you are going to store
-    // and how likely you allow collisions.
-    this.size = size;
-    this.table = new Array(size);
+  return arr;
+};
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = arr.length - 1; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
   }
+  return arr;
+};
 
-  _calculateHash = (key) => {
-    return key % this.size;
-  };
-
-  insert = (key, value) => {
-    const hash = this._calculateHash(key);
-    const node = new Node(key, value);
-    if (this.table[hash] === undefined) {
-      this.table[hash] = node;
+const conquer = (arr, s, m, e) => {
+  const left = arr.slice(s, m + 1);
+  const right = arr.slice(m + 1, e + 1);
+  let i = 0;
+  let j = 0;
+  let k = s;
+  for (k; k < e + 1; k++) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      if (i === left.length - 1) {
+        left[i] = Number.MAX_SAFE_INTEGER;
+        i--;
+      }
+      i++;
     } else {
-      // when a collision occurs, do the following:
-      let current = this.table[hash];
-      while (current.next !== null) {
-        current = current.next;
+      arr[k] = right[j];
+      if (j === right.length - 1) {
+        right[j] = Number.MAX_SAFE_INTEGER;
+        j--;
       }
-      current.next = node;
+      j++;
     }
-  };
+  }
+};
 
-  search = (key) => {
-    const hash = this._calculateHash(key);
-    let node = this.table[hash];
-    if (node === undefined) {
-      return "empty";
-    }
-    while (true) {
-      if (node.key === key) {
-        return node;
-      } else {
-        if (node.next === null) {
-          return "do not exist";
-        }
-        node = node.next;
-      }
-    }
-  };
+const mergeSort = (arr, start, end) => {
+  const m = Math.floor((start + end) / 2);
+  if (start < end) {
+    mergeSort(arr, start, m);
+    mergeSort(arr, m + 1, end);
+    conquer(arr, start, m, end);
+  }
+};
 
-  delete = (key) => {
-    const hash = this._calculateHash(key);
-    const targetNode = this.search(key);
-    if (this.table[hash] === undefined) {
-      return "Exception: Trying to delete non-existing node";
-    }
-    let current = this.table[hash];
-    let prev;
-    while (true) {
-      if (this.table[hash] === targetNode) {
-        this.table[hash] = this.table[hash].next;
-        break;
-      } else {
-        prev = current;
-        current = current.next;
-        if (current === targetNode) {
-          prev.next = targetNode.next;
-          break;
-        }
+const quickSort = (arr, start, end) => {
+  if (start < end) {
+    const pivot = arr[end];
+    let i = start;
+    for (let j = start; j < end; j++) {
+      if (arr[j] <= pivot) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
       }
     }
-  };
+    const t = arr[i];
+    arr[i] = pivot;
+    arr[end] = t;
+    quickSort(arr, start, i - 1);
+    quickSort(arr, i + 1, end);
+  }
+};
 
-  display = () => {
-    console.log("--------------------------------------");
-    for (let i = 0; i < this.table.length; i++) {
-      if (this.table[i] === undefined) {
-        console.log(`${i}: undefined`);
-      } else {
-        let node = this.table[i];
-        let row = i + ": ";
-        while (true) {
-          row += `{key: ${node.key},val: ${node.val}}`;
-          if (node.next === null) {
-            break;
-          }
-          row += " => ";
-          node = node.next;
-        }
-        console.log(row);
-      }
-    }
-    console.log("--------------------------------------");
-  };
-}
-
-const hashTable = new HashTable(10);
-
-hashTable.insert(3, "Alice");
-hashTable.insert(12, "Bob");
-hashTable.insert(233, "Chris");
-hashTable.insert(95, "David");
-hashTable.insert(183, "Eav");
-hashTable.insert(25, "George");
-
-hashTable.display();
-const x = hashTable.search(3);
-console.log(x);
-hashTable.delete(3);
-hashTable.display();
+const array1 = [9, 5, 2, 1, 7, 3, 4, 6, 8, 0];
+const array2 = [100, 100, 101, 220, 2, 1, 22, 33, 3, 1000];
+// const is = insertionSort(array);
+// const is2 = insertionSort(array2);
+// const bs = bubbleSort(array);
+// const bs2 = bubbleSort(array2);
+quickSort(array2, 0, 9);
+console.log(array2);
