bd97371fbbca8a2d053b6595d302037cd0a1bb83
Yuya Hochi <yuya1charo@gmail.com>
2022-10-13 14:41:18 +0100

    test

--- practice.js
+++ practice.js
@@ -5,7 +5,8 @@
 const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     for (let j = i - 1; j >= 0; j--) {
-      if (arr[j] > arr[i]) {
+      if (arr[j] > arr[i]) {  
+        
         const temp = arr[j];
         arr[j] = arr[i];
         arr[j + 1] = temp;
